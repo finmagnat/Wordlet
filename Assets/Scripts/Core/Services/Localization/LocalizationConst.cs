56f0776a87b1439438ea44093b231c6e150c57ce
Yurii Kobyliatskyi <finmagnat@hotmail.com>
2026-03-12 14:27:33 +0200

    Popup redesign.

--- Assets/Scripts/Core/Services/Localization/LocalizationConst.cs
+++ Assets/Scripts/Core/Services/Localization/LocalizationConst.cs
@@ -9,6 +9,8 @@ namespace Core.Services
         // Ключи:
         public const string KeyLabelScore = "LABEL_SCORE";
         public const string KeyLabelPasses = "LABEL_PASSES";
+        public const string KeyTableTextScore = "TABLE_TEXT_SCORE";
+        public const string KeyTableTextPasses = "TABLE_TEXT_PASSES";
         public const string KeyLabelLoading = "LOADING_TEXT";
         public const string KeyPopupSavedGameText = "POPUP_SAVED_GAME_TEXT";
         public const string KeyTextYes = "TEXT_YES";

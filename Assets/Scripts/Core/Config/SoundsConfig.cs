0b5eb4382906bd3b7343e9eede23a9bfaa4f3991
Yurii Kobyliatskyi <finmagnat@hotmail.com>
2026-08-23 15:39:13 +0300

    Sound config has been fixed.

--- Assets/Scripts/Core/Config/SoundsConfig.cs
+++ Assets/Scripts/Core/Config/SoundsConfig.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Core.Generated;
+using Unity.Collections;
 using UnityEngine;
 
 namespace Core.Config
@@ -10,25 +11,25 @@ namespace Core.Config
     {
         public static string StartNewGame => nameof(AssetKey.sfx_start_new_game);
         public static string LetterPutSuccess => nameof(AssetKey.sfx_letter_put_success);
-        public static string LetterSelected => nameof(AssetKey.sfx_letter_put_success);
+        public static string LetterSelected => nameof(AssetKey.sfx_letter_selected);
         public static string LetterUnblinking => nameof(AssetKey.sfx_letter_unblinking);
-        public static string LetterBlinking => nameof(AssetKey.sfx_letter_unblinking);
-        public static string IMadeMove => nameof(AssetKey.sfx_letter_put_success);
+        public static string LetterBlinking => nameof(AssetKey.sfx_letter_blinking);
+        public static string IMadeMove => nameof(AssetKey.sfx_i_made_move);
         public static string OpponentMadeMove => nameof(AssetKey.sfx_opponent_made_move);
         public static string OpponentWon => nameof(AssetKey.sfx_opponent_won);
         public static string PopupQuestion => nameof(AssetKey.sfx_popup_question);
-        public static string PopupWarning => nameof(AssetKey.sfx_popup_question);
-        public static string OpponentFindWordFail => nameof(AssetKey.sfx_popup_question);
-        public static string Pause => nameof(AssetKey.sfx_letter_put_success);
+        public static string PopupWarning => nameof(AssetKey.sfx_popup_warning);
+        public static string OpponentFindWordFail => nameof(AssetKey.sfx_opponent_find_word_fail);
+        public static string Pause => nameof(AssetKey.sfx_pause);
         public static string Pass => nameof(AssetKey.sfx_pass);
-        public static string Draw => nameof(AssetKey.sfx_opponent_won);
-        public static string IWon => nameof(AssetKey.sfx_opponent_won);
+        public static string Draw => nameof(AssetKey.sfx_draw);
+        public static string IWon => nameof(AssetKey.sfx_i_won);
         public static string SkinChanged => nameof(AssetKey.sfx_skin_changed);
         public static string ButtonClick => nameof(AssetKey.sfx_button_click);
         public static string BoosterFoundWord => nameof(AssetKey.sfx_booster_found_word);
-        public static string BoosterNotFoundWord => nameof(AssetKey.sfx_popup_question);
-        public static string BoosterSlowdownLaunch => nameof(AssetKey.sfx_booster_found_word);
-        public static string ShowRewardPopup => nameof(AssetKey.sfx_popup_reward);
+        public static string BoosterNotFoundWord => nameof(AssetKey.sfx_booster_not_found_word);
+        public static string BoosterSlowdownLaunch => nameof(AssetKey.sfx_booster_slowdown_launch);
+        public static string PopupReward => nameof(AssetKey.sfx_popup_reward);
        
         
         [TextArea]
@@ -46,8 +47,6 @@ namespace Core.Config
             new SoundData{ Key = IMadeMove, Description = "Игрок сделал ход" },
             new SoundData{ Key = OpponentMadeMove, Description = "Оппонент сделал ход" },
             new SoundData{ Key = OpponentWon, Description = "Выиграл оппонент" },
-            new SoundData{ Key = PopupQuestion, Description = "Попап с вопросом" },
-            new SoundData{ Key = PopupWarning, Description = "Отображение попапа 'Совет'" },
             new SoundData{ Key = OpponentFindWordFail, Description = "Оппонент не нашел букву и время вышло (пропуск хода)" },
             new SoundData{ Key = Pause, Description = "Пауза (квл/выкл)" },
             new SoundData{ Key = Pass, Description = "Игрок пропустил ход" },
@@ -58,7 +57,9 @@ namespace Core.Config
             new SoundData{ Key = BoosterFoundWord, Description = "Бустер 'Буковка' нашел слово" },
             new SoundData{ Key = BoosterNotFoundWord, Description = "Бустер 'Буковка' не нашел слово" },
             new SoundData{ Key = BoosterSlowdownLaunch, Description = "Запуск бустера 'Замедление'" },
-            new SoundData{ Key = ShowRewardPopup, Description = "Попап с наградой'" },
+            new SoundData{ Key = PopupReward, Description = "Попап с наградой'" },
+            new SoundData{ Key = PopupQuestion, Description = "Попап с вопросом" },
+            new SoundData{ Key = PopupWarning, Description = "Отображение попапа 'Совет'" },
         };
 
         
@@ -67,6 +68,7 @@ namespace Core.Config
     [Serializable]
     public struct SoundData
     {
+        [ReadOnly]
         public string Key;
         public string Description;
         public AudioClip Clip;

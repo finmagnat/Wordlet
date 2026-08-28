c745b02470b5f6ec8f430169be6d4c875bdebea9
Yurii Kobyliatskyi <finmagnat@hotmail.com>
2026-08-28 13:13:42 +0300

    Added AdvertisingBooster to round finish pop-ups.

---
+++ Assets/Scripts/UI/Components/AdvertisingBooster.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.UI.Components
+{
+    public class AdvertisingBooster : MonoBehaviour
+    {
+        [SerializeField] protected TextMeshProUGUI _labelText;
+        [SerializeField] protected TextMeshProUGUI _countText;
+        [SerializeField] protected Image _boosterIcon;
+    }
+}
